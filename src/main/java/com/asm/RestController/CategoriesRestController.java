2e3f6f9ca97f0a7620664ec42902fabacbef3885
Kiee <giakietgx@gmail.com>
2023-08-09 07:01:36 +0700

    category manage
    push chuc nang manage category

--- src/main/java/com/asm/RestController/CategoriesRestController.java
+++ src/main/java/com/asm/RestController/CategoriesRestController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +27,18 @@ public class CategoriesRestController {
 	public List<Category> getAll() {
 		return categoryService.findAll();
 	}
+	
+    @PutMapping("{id}")
+    public Category update(@PathVariable("id") String id,@RequestBody Category category) {
+        return categoryService.update(category);
+    }
+    @PostMapping("")
+    public Category create(@RequestBody Category category) throws Exception {
+        return categoryService.create(category);
+    }
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") String id)  {
+    	categoryService.delete(id);
+    }
 
 }
